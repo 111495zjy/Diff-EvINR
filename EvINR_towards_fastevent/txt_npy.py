ffbbb64a1e7c752145d931547ac5fa19639907cb
zjy <2020011631@student.cup.edu.cn>
2025-09-08 23:01:15 +0100

    Update txt_npy.py

--- EvINR_towards_fastevent/txt_npy.py
+++ EvINR_towards_fastevent/txt_npy.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-txt_file_path = '/content/DiffPIR/ECD/shapes_6dof/events.txt'
-npy_file_path = '/content/DiffPIR/ECD/shapes_6dof/events.npy'
+txt_file_path = '/content/Diff-EvINR/ECD/slider_depth/events.txt'
+npy_file_path = '/content/Diff-EvINR/ECD/slider_depth/events.npy'
 
 H = 480  # 这里假设图像高度为 480，请根据实际情况修改
 

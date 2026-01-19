d21669eb9e7cb5111890b7c4ffab30533eb9248e
zjy <2020011631@student.cup.edu.cn>
2026-01-19 11:31:27 +0100

    Update txt_npy.py

--- EvINR_towards_fastevent/txt_npy.py
+++ EvINR_towards_fastevent/txt_npy.py
@@ -3,15 +3,16 @@ import numpy as np
 txt_file_path = '/content/Diff-EvINR/ECD/slider_depth/events.txt'
 npy_file_path = '/content/Diff-EvINR/ECD/slider_depth/events.npy'
 
-H = 480  # 这里假设图像高度为 480，请根据实际情况修改
+H = 480  # This assumes the image height is 480; please adjust according to your actual situation.
 
 events = []
 with open(txt_file_path, 'r') as f:
-    #next(f)  # 跳过第一行
+    #next(f)  # skip first line
     for line in f:
         t, x, y, p = map(float, line.strip().split())
-        #y = H - 1 - y  # 翻转 y 坐标
+        #y = H - 1 - y  # flip y coordinate
         events.append([t, x, y, p])
 
 events_array = np.array(events)
 np.save(npy_file_path, events_array)
+
